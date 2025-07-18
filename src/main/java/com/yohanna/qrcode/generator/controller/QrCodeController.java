6fda124daa8c6fedfaf36fdecdf09a754b285ee8
YohannaOliveira <yohannaoliveira50@gmail.com>
2025-07-17 22:44:32 -0300

    feat: inclusao de config S3, properties e dockerfile

--- src/main/java/com/yohanna/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/yohanna/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,8 @@ package com.yohanna.qrcode.generator.controller;
 
 import com.yohanna.qrcode.generator.dto.QrCodeRequestDto;
 import com.yohanna.qrcode.generator.dto.QrCodeResponseDto;
+import com.yohanna.qrcode.generator.service.QrCodeGeneratorService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,10 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/qrcode")
 public class QrCodeController {
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
     @PostMapping
     public ResponseEntity<QrCodeResponseDto> generateQrCode(@RequestBody QrCodeRequestDto request) {
-        return null;
+        try {
+            QrCodeResponseDto response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception exception) {
+            return ResponseEntity.status(500).body(new QrCodeResponseDto("Error generating QR code: " + exception.getMessage()));
+        }
     }
 }
